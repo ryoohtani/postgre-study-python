d27ed9a266b308fa5fce633f2c6fe97dc0e2bad2
ryoohtani <kazuinto64@gmail.com>
2023-08-15 16:58:12 +0900

    sqlの修正

--- core/backend/py/main.py
+++ core/backend/py/main.py
@@ -19,29 +19,34 @@ result = cursor.fetchone()
 print(result[0] + "に接続完了")
 
 # Pythonからpostgreへinsertする
-cursor.execute("""insert into test_table ("ID", "売上日", "社員ID", "商品分類", "商品名", "単価", "数量", "売上金額") values 
-               (300, '2023812', 'a003', '飲料', 'コーラ', 150, 200, 15000)
-""")
+cursor.execute("""insert into test_table(
+               "ID", "売上日", "社員ID", "商品分類", "商品名", "単価", "数量", "売上金額") 
+               values(
+               300, '2023812', 'a003', '飲料', 'コーラ', 150, 200, 15000)""")
 
-cursor.execute("""insert into test_table ("ID", "売上日", "社員ID", "商品分類", "商品名", "単価", "数量", "売上金額") values
+cursor.execute("""insert into test_table(
+               "ID", "売上日", "社員ID", "商品分類", "商品名", "単価", "数量", "売上金額") 
+               values
                (500, '2023815', 'a005', 'ゲーム機', '任天堂3DS', 15000, 600, 55000000)
 """)
 
 # update       
-cursor.execute("""update test_table set "社員ID" = 'abcdefg' where "社員ID" = 'a002'
-""")
+cursor.execute("""update test_table set "社員ID" = 'abcdef' where "社員ID" = 'a002'""")
 
 # delete
-cursor.execute("""delete from test_table where "社員ID" = 'a001'
-""")
+cursor.execute("""delete from test_table where "社員ID" = 'a001'""")
 
-basic_data.commit()
+# basic_data.commit()
 
 # Pythonからpostgreへselect
 cursor.execute("""select * from test_table""")
+# cursor.execute("""select * from test_table where "社員ID" = 'a006'""")
 datas = cursor.fetchall()
 for data_list in datas:
     print(data_list)
 
 cursor.close()
 basic_data.close()
+
+
+sqlalchemy
