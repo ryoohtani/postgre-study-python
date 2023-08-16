f0ed20dcabcffe5e238a09ce78417f6a623f1180
ryoohtani <kazuinto64@gmail.com>
2023-08-16 22:45:40 +0900

    modelの機能
    DBの接続情報とpsycopg2などの定義ファイル

---
+++ core/backend/py/models/connector_db.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+class DbConnecotor(object):
+    # DBの接続情報を定義
+    def __init__(self, user, password, host, port, dbname):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.dbname = dbname
+        # DB接続
+        self.connection = None
+        # カーソルを作成してDB操作
+        self.cursor = None
+
+    # psycopg2を用いてDB接続を行う
+    def pypost_connect(self):
+        self.psyconnect = psycopg2.connect(
+        user = self.user,
+        password = self.password,
+        host = self.host,
+        port = self.port,
+        dbname = self.dbname
+        )
+        self.cursor = self.connection.cursor()
