cc74aaa55fddcb4f2c2a61c93bd3445feff5e96d
ryoohtani <kazuinto64@gmail.com>
2023-08-18 21:28:02 +0900

    テーブル作成のモジュール
    create文を記載している

--- core/backend/py/models/create_db.py
+++ core/backend/py/models/create_db.py
@@ -1,6 +1,19 @@
+# from models.connector_db import DbConnecotor
 
 class DbCreate(object):
     def __init__(self, basic_data):
         self.basic_data = basic_data
-    # ここから作成すること
-    def learning_table():
+
+    def learning_table(self):
+        cleate_learning_table = """
+            CREATE TABLE IF NOT EXISTS learning_table(
+                EMPLOYEE_CODE varchar(10) NOT NULL,
+                POSITION_NO varchar(5) NOT NULL,
+                GENDER varchar(1),
+                BASE_SALARY int,
+                DATE_OF_HIRE date,
+                PRIMARY KEY(EMPLOYEE_CODE)
+            )
+        """
+        self.basic_data.cursor.execute(cleate_learning_table)
+        self.basic_data.psyconnect.commit()
