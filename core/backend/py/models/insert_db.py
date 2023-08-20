2aec1e502e847a0e590fc5a5515b647bb5ecb3f3
ryoohtani <kazuinto64@gmail.com>
2023-08-20 22:03:38 +0900

    insert_db.pyにソースコードの追加
    対象テーブルの全てのデータを削除しないと一意制約エラーが生じるのを修正しました

--- core/backend/py/models/insert_db.py
+++ core/backend/py/models/insert_db.py
@@ -3,12 +3,18 @@ class DbInsert(object):
         self.basic_data = basic_data
 
     def insert_data(self):
+        insert_employee_code = ['s001', 's002', 's003', 's004']
+
+        delete_employee = """
+            delete from learning_table where EMPLOYEE_CODE = %s
+        """
+
         insert_employee = """
             insert into learning_table (
                 EMPLOYEE_CODE, POSITION_NO, GENDER, BASE_SALARY, DATE_OF_HIRE
             ) values (%s, %s, %s, %s, %s)
         """
-
+        
         employee_info = [
             ('s001', '1', '男', '210000', '2023-8-01'),
             ('s002', '2', '女', '220000', '2023-8-10'),
@@ -16,5 +22,37 @@ class DbInsert(object):
             ('s004', '4', '男', '240000', '2023-8-20')
         ]
 
-        self.basic_data.cursor.executemany(insert_employee, employee_info)
-        self.basic_data.psyconnect.commit()
+        for in_employee_code in insert_employee_code:
+            self.basic_data.cursor.execute(delete_employee, (in_employee_code,))
+            self.basic_data.psyconnect.commit()
+
+        for in_employee_info in employee_info:
+            self.basic_data.cursor.execute(insert_employee, in_employee_info)
+            self.basic_data.psyconnect.commit()
+
+    def insert_subdata(self):
+        subid_employee_code = ['s001', 's002', 's003', 's004']
+
+        delet_subemployee = """
+            delete from subid_table where EMPLOYEE_CODE = %s
+        """
+
+        insert_subemployee = """
+            insert into subid_table (
+                EMPLOYEE_CODE, EMPLOYEE_NAME, COUNTRY, COUNTRY_NO, ENROLLMENT_ID
+            ) values (%s, %s, %s, %s, %s)
+        """
+
+        subemployee_info = [
+            ('s001', '井上尚弥', 'JP', '21', '134'),
+            ('s002', '朝倉カンナ', 'JP', '21', '123'),
+            ('s003', 'グレゴリアン', 'ARM', '10', '200'),
+            ('s004', 'マクレガー', 'IRL', '2', '300')
+        ]
+        for employee_code in subid_employee_code:
+            self.basic_data.cursor.execute(delet_subemployee, (employee_code,))
+            self.basic_data.psyconnect.commit()
+        
+        for subemployee in subemployee_info:
+            self.basic_data.cursor.execute(insert_subemployee, subemployee)
+            self.basic_data.psyconnect.commit()
