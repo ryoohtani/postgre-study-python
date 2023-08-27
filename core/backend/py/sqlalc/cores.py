901989c5fc51264971ae7769b2fe20f10c6310aa
ryoohtani <kazuinto64@gmail.com>
2023-08-27 21:06:29 +0900

    cores.pyのリファクタリング
    命名規則やコメントなどを変更

--- core/backend/py/sqlalc/cores.py
+++ core/backend/py/sqlalc/cores.py
@@ -7,13 +7,13 @@ DATABASE_URL = "postgresql://admin:pypost@172.21.0.2/test_database"
 engine = sqlalchemy.create_engine(DATABASE_URL)
 
 # ORM(オブジェクトとRDB内のテーブルやレコードとの間でデータの変換や操作を自動的に行う)
-Base = sqlalchemy.orm.declarative_base()
+base = sqlalchemy.orm.declarative_base()
 
-# セッション
-Session = sqlalchemy.orm.sessionmaker(bind = engine)
-session = Session()
+# DBのテーブルからデータを取得するためにセッションを作成
+session = sqlalchemy.orm.sessionmaker(bind = engine)
+ses = session()
 
-class DbConnecotor(Base):
+class DbConnecotor(base):
     __tablename__ = "learning_table"
 
     EMPLOYEE_CODE = sqlalchemy.Column(sqlalchemy.VARCHAR(10), primary_key = True, nullable = False)
@@ -22,5 +22,5 @@ class DbConnecotor(Base):
     BASE_SALARY = sqlalchemy.Column(sqlalchemy.Integer)
     DATE_OF_HIRE = sqlalchemy.Column(sqlalchemy.Date)
 
-# テーブルをデータベースに作成
-Base.metadata.create_all(engine)
+# DbConnecotorに定義している情報を元にテーブル作成
+base.metadata.create_all(engine)
